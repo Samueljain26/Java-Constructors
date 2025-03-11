92d44cae7d5051cc1e40abe7c360244124f91917
Samueljain26 <samueljain77@gmail.com>
2025-03-11 10:47:37 +0530

    [Samuel] ADD: Cloning person details.

--- Person.java
+++ Person.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+class Person {
+    String name;
+    int age;
 
-public class Person {
+    // Parameterized Constructor
+    Person(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    // Copy Constructor (Cloning another Person)
+    Person(Person p) {
+        name = p.name;
+        age = p.age;
+    }
+
+    // Method to display Person details
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        // Creating the first Person object
+        Person p1 = new Person("Samuel", 21);
+        System.out.println("Original Person:");
+        p1.display();
+
+        System.out.println();
+
+        // Creating a second Person object by copying p1
+        Person p2 = new Person(p1);
+        System.out.println("Cloned Person:");
+        p2.display();
+    }
 }
+/*
+Original Person:
+Name: Samuel
+Age: 21
+
+Cloned Person:
+Name: Samuel
+Age: 21
+ */
